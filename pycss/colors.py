4093db5b720da69802dd6fddba3d21c5d9a975e8
Kristian <klette@samfundet.no>
2010-06-11 13:25:24 +0200

    Added placeholder colors

---
+++ pycss/colors.py
@@ -0,0 +1,14 @@
+
+
+tango_blue1 = '#123456'
+tango_blue2 = '#123456'
+tango_blue3 = '#123456'
+tango_blue4 = '#123456'
+tango_red1 = '#123456'
+tango_red2 = '#123456'
+tango_red3 = '#123456'
+tango_red4 = '#123456'
+tango_yellow1 = '#123456'
+tango_yellow2 = '#123456'
+tango_yellow3 = '#123456'
+tango_yellow4 = '#123456'
