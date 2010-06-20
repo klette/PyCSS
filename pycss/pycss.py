f9b3f5d641a4a06452c539008849bcf442e46311
Kristian <klette@samfundet.no>
2010-06-21 00:05:41 +0200

    Remove use of recursion

--- pycss/pycss.py
+++ pycss/pycss.py
@@ -2,26 +2,25 @@
 class PyCSS(object):
 
     @classmethod
-    def parse(cls, node, parents = None):
+    def parse(cls, startnode, parents = None):
         result = []
-        nodes_to_parse = []
+        nodes_to_parse = [([], startnode)]
 
-        if parents:
-            result.append("%s { " % " ".join(parents))
-        else:
-            parents = []
+        while nodes_to_parse:
+            parents, node = nodes_to_parse.pop(0)
+            if parents:
+                result.append("%s { " % " ".join(parents))
+            else:
+                parents = []
 
-        for key, value in node.iteritems():
-            if value.__class__.__name__ in ('str', 'unicode'):
-                result.append('%s: %s; ' % (key, value))
-            elif value.__class__.__name__ == 'function':
-               result.append('%s: %s; ' % (key, value()))
-            elif value.__class__.__name__ == 'dict':
-                nodes_to_parse.append((key, value))
-        if result:
-            result.append("}\n")
-
-        for n in nodes_to_parse:
-            result.append(PyCSS.parse(n[1], [p for p in parents] + [n[0]]))
+            for key, value in node.iteritems():
+                if value.__class__.__name__ in ('str', 'unicode'):
+                    result.append('%s: %s; ' % (key, value))
+                elif value.__class__.__name__ == 'function':
+                   result.append('%s: %s; ' % (key, value()))
+                elif value.__class__.__name__ == 'dict':
+                    nodes_to_parse.append(([p for p in parents ] + [key], value))
+            if result:
+                result.append("}\n")
 
         return "".join(result)
